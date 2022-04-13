5108ce853b13e4e4a6ff3c094331a46bbcd22098
ondirazamatov <ahdiir@gmail.com>
2022-04-14 02:02:43 +0600

    change car.py and base.py and plane.py

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -7,8 +7,8 @@ from exceptions import CargoOverload
 
 
 class Plane(Vehicle):
-    cargo = 0
-    max_cargo = 0
+    cargo = 3
+    max_cargo = 80
 
     def __init__(self, weight, fuel, fuel_consumption, max_cargo=max_cargo):
         super().__init__(weight, fuel, fuel_consumption)
