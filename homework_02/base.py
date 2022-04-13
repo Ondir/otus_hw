c6ce40591b4a785ef44287bc10ce62a63f92e670
ondirazamatov <ahdiir@gmail.com>
2022-04-14 02:13:13 +0600

    change car.py and base.py and plane.py

--- homework_02/base.py
+++ homework_02/base.py
@@ -18,8 +18,8 @@ class Vehicle(ABC):
             if self.fuel > 0:
                 self.started = True
                 return
-            elif self.fuel==0:
-                raise LowFuelError(self.started)
+            elif self.fuel == 0:
+                raise LowFuelError()
 
     def move(self, distance):
         max_distance = self.fuel // self.fuel_consumption
